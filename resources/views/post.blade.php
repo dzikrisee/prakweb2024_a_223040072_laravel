8c111799c8304b749e118e6b03fe838ce0cfe5eb
dzikrisee <marlianawatiazzahra@gmail.com>
2024-10-22 01:49:09 +0700

    Video #14 N+1 Problem

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -79,22 +79,8 @@ URL: https://flowbite.com/docs/components/typography/
                 </li>
             </ol> --}}
             
-            {{-- <section class="not-format">
-                <div class="flex justify-between items-center mb-6">
-                    <h2 class="text-lg lg:text-2xl font-bold text-gray-900 dark:text-white">Discussion (20)</h2>
-                </div>
-                <form class="mb-6">
-                    <div class="py-2 px-4 mb-4 bg-white rounded-lg rounded-t-lg border border-gray-200 dark:bg-gray-800 dark:border-gray-700">
-                        <label for="comment" class="sr-only">Your comment</label>
-                        <textarea id="comment" rows="6"
-                            class="px-0 w-full text-sm text-gray-900 border-0 focus:ring-0 dark:text-white dark:placeholder-gray-400 dark:bg-gray-800"
-                            placeholder="Write a comment..." required></textarea>
-                    </div>
-                    <button type="submit"
-                        class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
-                        Post comment
-                    </button>
-                </form>
+            <section class="not-format">
+                
                 <article class="p-6 mb-6 text-base bg-white rounded-lg dark:bg-gray-900">
                     <footer class="flex justify-between items-center mb-2">
                         <div class="flex items-center">
@@ -292,7 +278,7 @@ URL: https://flowbite.com/docs/components/typography/
                         </button>
                     </div>
                 </article>
-            </section> --}}
+            </section>
         </article>
     </div>
   </main>
